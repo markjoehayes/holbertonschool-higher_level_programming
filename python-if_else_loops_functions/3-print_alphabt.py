6728ee7164a15129dd0231e0e5e0857acbc98169
markjoehayes <markjoehayes@proton.me>
2025-05-05 14:02:46 -0400

    filename correction

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-	if letter == 101:
-		continue
-	if letter == 113:
-		continue
-	print("{}".format(chr(letter)), end="")
+    if letter == 101:
+        continue
+    if letter == 113:
+        continue
+    print("{}".format(chr(letter)), end="")
