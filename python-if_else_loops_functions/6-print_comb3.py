39fbf07a494493aaa44f4e4b7d1f499452f54887
markjoehayes <markjoehayes@proton.me>
2025-05-08 13:46:50 -0400

    chekcer isn't checking

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@
 for i in range(0, 8):
     for j in range(i, 10):
         if i != j:
-            print("{:d}{:d}, ".format(i, j), end='')
+            print("{}{}, ".format(i, j), end='')
 print("89")
