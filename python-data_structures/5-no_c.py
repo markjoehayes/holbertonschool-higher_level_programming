a87ab42620c236aeb8cdd9d59966ea793fc0d7ee
markjoehayes <markjoehayes@proton.me>
2025-05-12 15:56:41 -0400

    no c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    string = ""
-    for i in my_string[:]:
-        if i != 'c' and i != 'C':
-            string += i
-        return string
+    s = ""
+    copy = list(my_string)
+    for i in range(len(my_string)):
+        if my_string[i] == 'c':
+            copy.remove('c')
+        if my_string[i] == 'C':
+            copy.remove('C')
+    return s.join(copy)
