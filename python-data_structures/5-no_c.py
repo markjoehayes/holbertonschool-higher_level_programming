24917060cd3cd1fb142169ac36329afae7425bae
markjoehayes <markjoehayes@proton.me>
2025-05-12 15:51:32 -0400

    no c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    print(my_string.translate({ord(i): "" for i in 'cC'}))
+    string = ""
+    for i in my_string:
+        if i != 'c' or i != 'C':
+            string += i
+        return string
