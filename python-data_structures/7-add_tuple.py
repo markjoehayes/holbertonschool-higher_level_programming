691fae48ea5f4717f67fbe0f7b3569684f10fcd1
markjoehayes <markjoehayes@proton.me>
2025-05-13 13:13:36 -0400

    adjusted for tuples less than 2

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    list_a = list(tuple_a)
+    list_b = list(tuple_b)
+    while len(list_a) < 2:
+        list_a.append(0)
+    while len(list_b) < 2:
+        list_a.append(0)
+    return tuple([list_a[0] + list_b[0], list_a[1] + list_b[1]])
