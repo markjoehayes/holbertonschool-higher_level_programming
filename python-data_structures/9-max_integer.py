75e21d458d914cac1839e67458ea6116a6f14db0
markjoehayes <markjoehayes@proton.me>
2025-05-13 13:43:07 -0400

    max

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    for i in range(len(my_list) - 1):
+        if my_list[i] > my_list[i +1]:
+            tmp = my_list[i]
+            my_list[i] = my_list[i + 1]
+            my_list[i + 1] = tmp
+    return my_list[-1]
