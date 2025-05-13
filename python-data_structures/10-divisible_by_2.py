7c09d7b163ddb418f1029f4d5980a907cb98515d
markjoehayes <markjoehayes@proton.me>
2025-05-13 13:56:50 -0400

    odd/even

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     list_result = []
     for i in my_list:
-        if i  % 2 is 0:
+        if i % 2 is 0:
             list_result += [True]
         else:
             list_result += [False]
