c3e19c28df7263d7d325f101adf1083206e284b5
markjoehayes <markjoehayes@proton.me>
2025-05-13 13:31:07 -0400

    more tuples

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,5 +2,6 @@
 def multiple_returns(sentence):
     if sentence == "":
         first = None
-    else first = sentence[0]
+    else:
+        first = sentence[0]
     return (len(sentence), first)
