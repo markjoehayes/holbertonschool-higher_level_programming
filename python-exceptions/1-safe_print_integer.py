4b96449004ac238c27d52677953dde66ca321b88
markjoehayes <markjoehayes@proton.me>
2025-05-18 20:20:43 -0400

    try

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, bool):
-            return False
-        number = int(value)
         print("{:d}".format(number))
         return True
     except (ValueError, TypeError):
