038bcafaedae8c5095c7014afe235d3387a0b4fc
markjoehayes <markjoehayes@proton.me>
2025-05-21 15:42:30 -0400

    fix pedantic messages mistakes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,8 +7,8 @@ class Square:
     def __init__(self, size=0):
         """initialize the data"""
         if type(size) != int:
-            raise TypeError ('size must be an integer')
+            raise TypeError('size must be an integer')
         if size < 0:
-            raise ValueError ('size must be >= 0')
+            raise ValueError('size must be >= 0')
         else:
             self.__size = size
