adf4d94efbd936b7f9fb969de82ffd27edccdeac
markjoehayes <markjoehayes@proton.me>
2025-05-23 09:26:39 -0400

    added space

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""a class to define a rectangle"""
+
+
+class Rectangle:
+    """Start class Rectangle"""
+    def __init__(self, width=0, height=0):
+        """initialize the data"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """get width of rectangle"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """get height of the rectangle"""
+        return (self, __height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
