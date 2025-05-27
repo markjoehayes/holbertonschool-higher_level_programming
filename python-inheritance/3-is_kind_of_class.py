79f3d3d7ddb2fd2f81e52d174edfbe332d5585fe
markjoehayes <markjoehayes@proton.me>
2025-05-27 15:12:50 -0400

    isinstance-inherited

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Is instance of"""
+def is_kind_of_class(obj, a_class):
+    """Test if obj is an instance of or an instance of inhertince"""
+    return (isinstance(obj, a_class))
