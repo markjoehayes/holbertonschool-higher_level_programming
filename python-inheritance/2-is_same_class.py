85f1fbbd0973630720dbd69b9784dd8026c928eb
markjoehayes <markjoehayes@proton.me>
2025-05-27 15:03:09 -0400

    diff function

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,4 +2,4 @@
 """test if object is exactly an instance of the specified class"""
 def is_same_class(obj, a_class):
     """is object instance of class"""
-    return (isinstance(obj, a_class))
+    return (type(obj) is a_class)
