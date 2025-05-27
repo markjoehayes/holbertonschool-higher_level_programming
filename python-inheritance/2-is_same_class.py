fca5880e3404e68c66f571b4446758787863ca2e
markjoehayes <markjoehayes@proton.me>
2025-05-27 14:57:39 -0400

    isinstance

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,5 @@
+#!/usr/bin/pythonm3
+"""test if object is exactly an instance of the specified class"""
+def is_same_class(obj, a_class):
+    """is object instance of class"""
+    return (isinstance(obj, a_class))
