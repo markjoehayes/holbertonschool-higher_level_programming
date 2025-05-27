ee4793d3c3001f5c177d0ec4d10e41413f54f773
markjoehayes <markjoehayes@proton.me>
2025-05-27 14:06:56 -0400

    inherit from list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module with class MyList"""
+
+class MyList(list):
+    """Class with method print_sorted"""
+    pass
+
+    def print_sorted(self):
+        """Method that sorted a list"""
+        print(sorted(list(self)))
