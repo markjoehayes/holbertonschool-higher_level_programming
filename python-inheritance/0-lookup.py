a4b5aefaa6caa43ef8abf8c3ca5a9f7353a5ae6d
markjoehayes <markjoehayes@proton.me>
2025-05-27 13:59:21 -0400

    added blank lines

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """return attributes and methods of an object"""
+
+
 def lookup(obj):
     """function to return all the attributes and methods of an object"""
     return (list(dir(obj)))
