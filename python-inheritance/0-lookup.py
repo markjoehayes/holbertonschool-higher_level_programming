ca7e10fd37112ec764110c795d3e67d4648ff177
markjoehayes <markjoehayes@proton.me>
2025-05-27 13:58:29 -0400

    typo in shebang

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""return attributes and methods of an object"""
 def lookup(obj):
     """function to return all the attributes and methods of an object"""
     return (list(dir(obj)))
