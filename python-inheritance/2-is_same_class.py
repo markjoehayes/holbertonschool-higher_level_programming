49a3d0d4b12df43fdc371971611240dfa5ecbfc9
markjoehayes <markjoehayes@proton.me>
2025-05-27 15:04:04 -0400

    need 2 blank lines

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """test if object is exactly an instance of the specified class"""
+
+
 def is_same_class(obj, a_class):
     """is object instance of class"""
     return (type(obj) is a_class)
