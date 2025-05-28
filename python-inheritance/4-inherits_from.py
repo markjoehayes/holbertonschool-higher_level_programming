6f700baa43968ca514f61cf400ae6ca3d31964af
markjoehayes <markjoehayes@proton.me>
2025-05-28 06:26:18 -0400

    direct/indirect instance

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""is object an oinstance of a class that is inherited"""
+def inherits_from(obj, a_class):
+    """Returns True if object is a direct or indirect instance of class"""
+    return (isinstance(obj, a_class))
