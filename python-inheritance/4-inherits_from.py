a3a6514ebce4fc1348c02cafb93f31d96b171116
markjoehayes <markjoehayes@proton.me>
2025-05-28 06:32:40 -0400

    added test to verify obj is subclass

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """is object an oinstance of a class that is inherited"""
+
+
 def inherits_from(obj, a_class):
     """Returns True if object is a direct or indirect instance of class"""
-    return (isinstance(obj, a_class))
+    return (isinstance(obj, a_class) and type(obj) is not a_class)
