eaa5ac7065c1792913d275581cdb93c07002b4eb
markjoehayes <markjoehayes@proton.me>
2025-05-30 10:34:15 -0400

    string rep aquare

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Modul for Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A subclass representing a Square"""
+    def __init__(self, size):
+        """Constructor"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """returns the area of square"""
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """String Representation Method"""
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
