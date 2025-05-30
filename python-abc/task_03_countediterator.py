df058df22be1cd10dce437f49f5a1adfe1236e6b
markjoehayes <markjoehayes@proton.me>
2025-05-30 15:18:20 -0400

    counted iterator

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+class CountedIterator:
+    """An iterator that keeps track of the number of items iterated over."""
+
+    def__init__(self, iterable):
+        """Initialize the iterator and counter."""
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __next__(self):
+        """Return the next item and increment the counter."""
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise StopIteration
+
+    def get_count(self):
+        """Return the current count of items iterated over."""
+        return self.count
+
+    def __iter__(self):
+        """Return the iterator itself."""
+        return self
