bf3de286c1d5359f631283beeff190fa0834d1f7
markjoehayes <markjoehayes@proton.me>
2025-06-01 21:58:23 -0400

    rebel int

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,10 @@
+!/usr/bin/python3
+
+class MyInt(int):
+    """A Class MyInt, a rebel integer"""
+    def __equal__(self, value):
+        """Change == to !="""
+        return self.real != value
+    def __notequal__(self, value):
+        """Change != to =="""
+        return self.real == value
