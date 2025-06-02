b524ef84058ff54326df7ad35bc369f995e2f933
markjoehayes <markjoehayes@proton.me>
2025-06-02 13:24:14 -0400

    open and print

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write a function that reads a text file"""
+
+
+def read_file(filename=""):
+    """Print the contents of a UTF-8 textfile to stdout"""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
