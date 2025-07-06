616496193981443af6f7b100289a749ddc81848d
markjoehayes <markjoehayes@proton.me>
2025-07-06 12:38:04 -0400

    filter by arg

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+list all states that match a given argument
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ = "__main__":
+    db = MySQLdb.coinnect(host ="localhost", port =3306,
+                            user=argv[1], passwd=argv[2], db=argv[3], charset="utf-8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
