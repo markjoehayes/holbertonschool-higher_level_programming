9b60807af3c96f82a5a805277806035df6af9e89
markjoehayes <markjoehayes@proton.me>
2025-07-08 11:09:36 -0400

    added backticks

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,12 +9,5 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host ="localhost", port =3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM `states` WHERE name = BINARY '{}' ORDRER BY id".format(argv[4]))
-    cur.execute(query)
-
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    cur.close()
-    db.close()
+    cur.execute("SELECT * FROM `states` WHERE BINARY `name` = '{}'".format(argv[4]))
+    [print(state) for state in cur.fetchall()]
