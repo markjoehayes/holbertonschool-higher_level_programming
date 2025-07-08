80a99e1988b6991cf9b6192d7c69afefcd99dbf8
markjoehayes <markjoehayes@proton.me>
2025-07-08 10:56:23 -0400

    changes sys.argv to argv

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host ="localhost", port =3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` WHERE BINARY `name` = '{}' ORDRER BY id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM `states` WHERE BINARY `name` = '{}' ORDRER BY id ASC".format(argv[4]))
     [print(state) for state in cur.fetchall()]
     cur.close()
     db.close()
