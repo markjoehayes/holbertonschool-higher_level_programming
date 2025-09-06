4cba1b3f068e98a7f40553fe5c97a9d0c2332c36
markjoehayes <markjoehayes@proton.me>
2025-09-06 16:45:43 -0400

    remove index

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -2,6 +2,5 @@
 def remove_char_at(str, n):
     new_str = ""
     for i in str:
-        new_str.append(str[:n])
-        new_str.append(str[n+1:])
+        new_str += (str[:n]) + (str[n+1:])
     return new_str
