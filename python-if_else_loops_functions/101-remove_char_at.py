bc3fed6c70bd576618288f8d041f05a5d0bd063f
markjoehayes <markjoehayes@proton.me>
2025-09-07 11:44:11 -0400

    combo

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -2,5 +2,7 @@
 def remove_char_at(str, n):
     new_str = ""
     for i in str:
-        new_str += (str[:n]) + (str[n+1:])
+        new_str += (str[:n]).join(str[n+1:])
     return new_str
+
+print(remove_char_at("chicago", 2))
