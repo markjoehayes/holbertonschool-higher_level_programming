c72381100636210d288f5b1364ac7e6c7aa47ef5
markjoehayes <markjoehayes@proton.me>
2025-09-09 15:12:52 -0400

    list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -5,4 +5,6 @@ def replace_in_list(my_list, idx, element):
         return my_list
     elif (idx >= len(my_list)):
         return my_list
-    return my_list.insert(idx, element)
+    else:
+        my_list[idx] = element
+    return my_list
