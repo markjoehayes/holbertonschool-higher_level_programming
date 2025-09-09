bb9e79823dd5d47d5b4cda615861d9ecb9c0ed91
markjoehayes <markjoehayes@proton.me>
2025-09-09 15:43:57 -0400

    remove c from string

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.remove(c)
-    another_string = new_string.remove(C)
-    return another_string
+    new_string = ""
+    for letter in my_string:
+        if (letter == 'c' or letter == 'C'):
+            continue
+        new_string.append(letter)
