facbe7e5af2d33b8c4044cf1393baf02e929bda2
markjoehayes <markjoehayes@proton.me>
2025-09-09 19:53:37 -0400

    complete

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a0 = tuple_a[0] if len(tuple_a) > 0 else 0
     a1 = tuple_a[1] if len(tuple_a) > 1 else 0
-    b0 = tuble_b[0] if len(tuble_b) > 0 else 0
+    b0 = tuple_b[0] if len(tuple_b) > 0 else 0
     b1 = tuple_b[1] if len(tuple_b) > 1 else 0
     new_tuple = (a0 + b0), (a1 + b)
     return new_tuple
