85802abd4cc4fb3d07647b24b577210d93503760
markjoehayes <markjoehayes@proton.me>
2025-09-09 15:33:35 -0400

    return list, not None when over

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -5,7 +5,7 @@ def new_in_list(my_list, idx, element):
     if (idx < 0):
         return my_list
     elif (idx >= len(my_list)):
-        return None
+        return my_list
     else:
         new_list[idx] = element
     return new_list
