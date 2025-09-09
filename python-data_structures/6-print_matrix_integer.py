4d002a799ae96ff33b6bda5968a443df0b819e04
markjoehayes <markjoehayes@proton.me>
2025-09-09 19:42:15 -0400

    first try, without conditionals

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    i = 0; j = 0
+    for i in ranfe
     print("{:d} {:d} {:d}".format(matrix[0][0], matrix[0][1], matrix[0][2]))
     print("{:d} {:d} {:d}".format(matrix[1][0], matrix[1][1], matrix[1][2]))
     print("{:d} {:d} {:d}".format(matrix[2][0], matrix[2][1], matrix[2][2]))
