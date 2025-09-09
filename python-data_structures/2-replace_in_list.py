6f9c44795d65c86e1947696c6f630cdf32841f16
markjoehayes <markjoehayes@proton.me>
2025-09-09 15:07:55 -0400

    insert method

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -5,4 +5,4 @@ def replace_in_list(my_list, idx, element):
         return my_list
     elif (idx >= len(my_list)):
         return my_list
-    return my_list[element, idx]
+    return my_list.insert(element,idx)
