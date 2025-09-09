7f8a6c4bcb491a6011af5ecf7f6ca0288bca175b
markjoehayes <markjoehayes@proton.me>
2025-09-09 19:34:34 -0400

    removed ,

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print("{:d}, {:d}, {:d}".format(matrix[0][0], matrix[0][1], matrix[0][2]))
-    print("{:d}, {:d}, {:d}".format(matrix[1][0], matrix[1][1], matrix[1][2]))
-    print("{:d}, {:d}, {:d}".format(matrix[2][0], matrix[2][1], matrix[2][2]))
+    print("{:d} {:d} {:d}".format(matrix[0][0], matrix[0][1], matrix[0][2]))
+    print("{:d} {:d} {:d}".format(matrix[1][0], matrix[1][1], matrix[1][2]))
+    print("{:d} {:d} {:d}".format(matrix[2][0], matrix[2][1], matrix[2][2]))
