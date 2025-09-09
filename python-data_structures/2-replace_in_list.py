9f976b4b4a6fefcb16737a8bc29f3ce98173941a
markjoehayes <markjoehayes@proton.me>
2025-09-09 15:05:11 -0400

    change value at index

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """Replace an elemnt at a specific location in list."""
+    if (idx < 0):
+        return my_list
+    elif (idx >= len(my_list)):
+        return my_list
+    return my_list[element, idx]
