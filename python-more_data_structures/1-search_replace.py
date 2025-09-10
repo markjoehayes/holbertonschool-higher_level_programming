38a9808c8ecd61e02811e187c92bd979ce6ef397
markjoehayes <markjoehayes@proton.me>
2025-09-10 08:33:44 -0400

    name error mylist

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(mylist)):
-        if search == mylist[i]:
-            mylist[i] = replace
-    return mylist
+    for i in range(len(my_list)):
+        if search == my_list[i]:
+            my_list[i] = replace
+    return my_list
