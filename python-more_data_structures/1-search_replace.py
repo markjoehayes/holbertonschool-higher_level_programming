35f3a86e9a9c4e35a6f920b35b094bdaf6cbf44a
markjoehayes <markjoehayes@proton.me>
2025-09-10 08:32:19 -0400

    search/replace

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for i in range(len(mylist)):
+        if search == mylist[i]:
+            mylist[i] = replace
+    return mylist
