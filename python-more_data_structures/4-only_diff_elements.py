60b4de7fddd473c59f47cab25fb51e15cfb899bf
markjoehayes <markjoehayes@proton.me>
2025-09-11 09:20:10 -0400

    union

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    differents = set()
-    for element in set_1:
-        differents.add(element)
-    for elemtts in set_2:
-        differents.add(element)
-    return differents
+    return set_1.union(set_2)
