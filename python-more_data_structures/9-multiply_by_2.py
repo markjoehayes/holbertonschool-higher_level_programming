06d8d3dbb3f3b5f5048053153a5b48a2e68fb08f
markjoehayes <markjoehayes@proton.me>
2025-09-11 13:47:42 -0400

    copy new dict

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dict = {}
     for key, item in a_dictionary.items():
-        a_dictionary[key] = item * 2
-    return a_dictionary
+        new_dict[key] = item * 2
+    return new_dict
