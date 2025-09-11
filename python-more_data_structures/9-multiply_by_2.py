a9c7c29ca033975c6e1311c9404debc2f6e87a49
markjoehayes <markjoehayes@proton.me>
2025-09-11 13:43:36 -0400

    finally

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     for key, item in a_dictionary.items():
-        a_dictionary[item] = item * 2
+        a_dictionary[key] = item * 2
     return a_dictionary
