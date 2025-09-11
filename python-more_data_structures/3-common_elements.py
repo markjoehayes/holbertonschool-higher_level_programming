a0c67e0b09bfef30c53f3663984be7905f1a297c
markjoehayes <markjoehayes@proton.me>
2025-09-11 09:01:17 -0400

    replace append with add (for set)

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -3,6 +3,6 @@ def common_elements(set_1, set_2):
     common_elements = set()
     for element in set_1:
         if element in set_2:
-            common_elements.append(element)
+            common_elements.add(element)
     return common_elements
 
