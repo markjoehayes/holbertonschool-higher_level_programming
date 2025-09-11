bd1849cf1c7718f67f9bb5681c296acb1298766d
markjoehayes <markjoehayes@proton.me>
2025-09-11 09:08:51 -0400

    2nd try

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -4,4 +4,7 @@ def only_diff_elements(set1, set2):
     for element in set1:
         if element not in set2:
             differents.add(element)
+    for elements in set2:
+        if element not in differents:
+            differents.add(element)
     return differents
