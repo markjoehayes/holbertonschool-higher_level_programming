ceb1947e9eb1010c730f0a98fecf9550953024c9
markjoehayes <markjoehayes@proton.me>
2025-09-11 08:58:53 -0400

    common-elements

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -3,6 +3,6 @@ def common_elements(set_1, set_2):
     common_elements = set()
     for element in set_1:
         if element in set_2:
-            common_elements += element
+            common_elements.append(element)
     return common_elements
 
