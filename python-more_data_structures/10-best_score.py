83960a68c03b00e1021f314c4d25f5417ba86e3a
markjoehayes <markjoehayes@proton.me>
2025-09-11 14:12:17 -0400

    highscore

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    scores = list(a_dictionary.values())
-    i = 0
-    for score in scores:
-            high_score = score[i]
-            i += 1
-    return high_score
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+    best_key = None
+    best_value = None
+
+    for key, value in a_dictionary.items():
+        if best_value is None or value > best_value:
+            best_key = key
+            best_value = value
+    return best_key
+
