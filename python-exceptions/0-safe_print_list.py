9301a91229b3433ba2c71ea8e22e3f4233990219
markjoehayes <markjoehayes@proton.me>
2025-09-14 11:29:48 -0400

    variable correction

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     """Prints x elements in a list and returns real number of elemenst printed"""
-    i = 0
+    count = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
-            i += 1
+            count += 1
         except IndexError:
             break
     print()
