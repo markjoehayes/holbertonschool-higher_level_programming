ea59bcaa83073e332f36b7101272fe1eb1a86634
markjoehayes <markjoehayes@proton.me>
2025-09-16 09:06:10 -0400

    private attribute, size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/uar/bin/python3
+"""A program to define a Square"""
+
+
+class Square:
+    """A class that defines a square"""
+
+    def __init__(self, size):
+        """Sets the necessary attributes for the Square object"""
+        self.__size = size
