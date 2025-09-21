08e432b2937a9f9edc73cb975be469017ba48634
markjoehayes <markjoehayes@proton.me>
2025-09-21 11:45:43 -0400

    final!

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -4,40 +4,66 @@
 import sys
 
 
-def error_exit(message="", code=1):
-    """Handle exit code/message"""
-    print(message)
-    print(code)
+def main():
+    """Main function"""
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    
+    def is_safe(board, row, col):
+        """Check if position is safe for queen"""
+        # Check row on left side
+        for i in range(col):
+            if board[row][i] == 1:
+                return False
+        
+        # Check upper diagonal
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+        
+        # Check lower diagonal
+        for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+        
+        return True
+    
+    def solve(board, col, solutions):
+        """Backtracking solution"""
+        if col == N:
+            # Convert to required format
+            solution = []
+            for i in range(N):
+                for j in range(N):
+                    if board[i][j] == 1:
+                        solution.append([i, j])
+            solutions.append(solution)
+            return
+        
+        for i in range(N):
+            if is_safe(board, i, col):
+                board[i][col] = 1
+                solve(board, col + 1, solutions)
+                board[i][col] = 0  # backtrack
+    
+    board = [[0] * N for _ in range(N)]
+    solutions = []
+    solve(board, 0, solutions)
+    
+    for solution in solutions:
+        print(solution)
 
-if len(sys.argv) is not 2:
-    error_exit("Usage: nqueens N")
 
-try:
-    N = int(sys.argv[1])
-except:
-    error_exit("N must be a number")
-
-if N < 4:
-    error_exit("N must be at least 4")
-
-def test_pos(board, y):
-    """Test if a queen can be places at current position"""
-    for i in range(y):
-        if board[y][1] is board [i][i]:
-            return False
-        if abs(board[y][1] - board[i][i] == y - i):
-            return False
-    return True
-
-def ref_backtrack(board, y):
-    """Backtrack the possibilities"""
-    if y is N:
-        print(boared)
-    else:
-        for x in range(N):
-            board[y][1] = x
-            if test_pos(board, y):
-                rec_backtrack(board, y + 1)
-
-board = [[y, 0] for y in range(N)]
-rec_backtrack(board, 0)
+if __name__ == "__main__":
+    main()
