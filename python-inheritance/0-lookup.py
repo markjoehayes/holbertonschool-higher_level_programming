d5e1b9c2187f9414e748844b84eeb0edc6931bb4
markjoehayes <markjoehayes@proton.me>
2025-09-23 12:12:23 -0400

    added a blanl line for checker

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module to list avaiulable attributes and methods of an object"""
 
+
 def lookup(obj):
     """returns a listr of attriubutes and methods of an object"""
     return dir(obj)
