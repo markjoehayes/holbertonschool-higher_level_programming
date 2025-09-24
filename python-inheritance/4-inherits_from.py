96262179608476f3e0e95d06341683ce00dcc785
markjoehayes <markjoehayes@proton.me>
2025-09-24 06:11:00 -0400

    remove helpful comments b/c checker!

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,9 +4,7 @@
 
 def inherits_from(obj, a_class):
     """return true if the object is instance of an inherited class"""
-    #Get the class of the object
     obj_class = type(obj)
-    #Check if it is a subclass, but not the main class
     if obj_class is not a_class and issubclass(obj_class, a_class):
         return True
     return False
