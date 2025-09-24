6c6d00cd60c509d4f29876ecd6317339c2998423
markjoehayes <markjoehayes@proton.me>
2025-09-24 06:15:32 -0400

    empty class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,9 @@
 
 def inherits_from(obj, a_class):
     """return true if the object is instance of an inherited class"""
+    #Get the class of the object
     obj_class = type(obj)
+    #check if it is a subclass, but not the main class
     if obj_class is not a_class and issubclass(obj_class, a_class):
         return True
     return False
