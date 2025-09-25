d30589e7288ac027a731701f0137ad3a66ab0399
markjoehayes <markjoehayes@proton.me>
2025-09-25 13:52:20 -0400

    Myint 1st

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to create a rebel integer"""
+
+
+class MyInt(int):
+    """defines a rebel integer class called MyInt"""
+    
+    def __eq__(self, value):
+        """exchange == operator with != operator"""
+        return self.real != value
+
+    def __ne__(self, value):
+        return self.real == value
