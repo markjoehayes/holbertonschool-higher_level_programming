4926adb69b820ba6b537c53417d3968b66bf2d49
markjoehayes <markjoehayes@proton.me>
2025-10-01 08:22:38 -0400

    doc module

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module to open a file and print to stdout"""
+
+
 def read_file(filename=""):
     """Reads a text file and prints it to stdout."""
     with open(filename, 'r', encoding='utf-8') as file:
