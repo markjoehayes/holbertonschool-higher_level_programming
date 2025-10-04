8205a3289fc15dbddf18d71a2dd9b8313db48060
markjoehayes <markjoehayes@proton.me>
2025-10-04 08:45:29 -0400

    added encoding

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,6 +4,5 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    filename = json.dumps(my_obj)
-    with open("filename", "w") as f:
-        f.write(filename)
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
