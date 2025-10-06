a1a9071bc6c8b9fae011ac07fc939eac491a2033
markjoehayes <markjoehayes@proton.me>
2025-10-06 07:28:31 -0400

    load

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """function to create an object from a json file"""
     with open(filename, "r", encoding="utf-8") as f:
-        return json.loads(f)
+        return json.load(f)
