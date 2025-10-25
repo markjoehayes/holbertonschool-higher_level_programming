47a2a3d22c6ca03b0fd52cd1f5fdf599a8ff35d0
markjoehayes <markjoehayes@proton.me>
2025-10-25 08:53:12 -0400

    1st

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- 2-create__read_user.sql
+
+CREATE TABLE IF NOT EXISTS 'hbtn_0d_2'
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
