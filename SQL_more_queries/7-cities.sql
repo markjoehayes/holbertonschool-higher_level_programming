bd9635ccee468c06477f6ca08b8f6c97d7c2fc69
markjoehayes <markjoehayes@proton.me>
2025-10-25 09:37:53 -0400

    removed DATABASE from line4

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- 7-cities.sql
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE DATABASE hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL AUTO_INCREMENT PRIMARY_KEY,
     state_id INT NOT NULL,
