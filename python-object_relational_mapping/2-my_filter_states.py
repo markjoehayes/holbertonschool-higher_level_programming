2f1dc0827ae4b2855eba05f0c1a31cd118795065
markjoehayes <markjoehayes@proton.me>
2025-10-26 07:32:59 -0400

    removed a comma??

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], 
                          passwd=sys.argv[2], 
-                         db=sys.argv[3],
+                         db=sys.argv[3]
     )
     # create cursor object
     cur = db.cursor()
