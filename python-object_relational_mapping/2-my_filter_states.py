05c57b8cd878f25a5f4a9bcbb7bf00ba06af8d9c
markjoehayes <markjoehayes@proton.me>
2025-10-26 07:27:35 -0400

    1st

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa, where
+name matches the argument
+"""
+
+impore MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], 
+                         passwd=sys.argv[2], 
+                         db=sys.argv[3],
+                         stae_name=sys.argv[4])
+    # create cursor object
+    cur = db.cursor()
+    # execute the query
+    cur.execute("SELECT * FROM states WHERE name = state_name ORDER BY id ASC")
+    # fecth all rows
+    rows = cur.fetchall()
+    # print the results
+    for row in rows:
+        print(row)
+    # close the cursor and database connection    
+    cur.close()
+    db.close()
