b4a6c77cd64986e55e306f46b55ee612bdb1716e
markjoehayes <markjoehayes@proton.me>
2025-10-26 07:42:03 -0400

    remove line breaks to make it LESS readable

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,11 +9,8 @@ import sys
 
 if __name__ == "__main__":
     # connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], 
-                         passwd=sys.argv[2], 
-                         db=sys.argv[3]
-    )
+    db = MySQLdb.connect(host="localhost", port=3306, 
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     # create cursor object
     cur = db.cursor()
     # execute the query
