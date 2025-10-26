86da1de5e3532726afdd286818a058cc8e8743e2
markjoehayes <markjoehayes@proton.me>
2025-10-26 08:20:56 -0400

    1st

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, 
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    # create cursor object
+    cur = db.cursor()
+    # execute the query *with parameterized input*
+    cur.execute("SELECT * FROM cities ORDER BY id ASC"
+    # fecth all rows
+    rows = cur.fetchall()
+    # print the results
+    for row in rows:
+        print(row)
+    # close the cursor and database connection    
+    cur.close()
+    db.close()
