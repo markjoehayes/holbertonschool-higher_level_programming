2ecbdc50f5938f6ae998656cb911ae9f5d0cee17
markjoehayes <markjoehayes@proton.me>
2025-10-26 07:14:53 -0400

    1st

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa, staring with 'N'
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    # create cursor object
+    cur = db.cursor()
+    # execute the query
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY ASC")
+    # fecth all rows
+    rows = cur.fetchall()
+    # print the results
+    for row in rows:
+        print(row)
+    # close the cursor and database connection    
+    cur.close()
+    db.close()
