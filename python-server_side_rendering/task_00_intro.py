9bc9108a7263a100bb61048e41b252815cb3567d
markjoehayes <markjoehayes@proton.me>
2025-11-16 19:35:20 -0400

    app fac

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -2,27 +2,27 @@
 from os.path import exists
 
 
-def generate_invitations(template, attendees_list):
+def generate_invitations(template, attendees):
     """Function for generating invitations"""
 
     if not template:
         print("ERROR: template cannot be empty")
         return
 
-    if not attendees_list:
-        print("ERROR: attendees_list cannot be empty")
+    if not attendees:
+        print("ERROR: attendees cannot be empty")
         return
 
     if not isinstance(template, str):
         print("ERROR: template must be a string")
         return
 
-    if (not isinstance(attendees_list, list) or
-            not all(isinstance(item, dict) for item in attendees_list)):
-        print("ERROR: attendees_list must be a list of dictionaries")
+    if (not isinstance(attendees, list) or
+            not all(isinstance(item, dict) for item in attendees)):
+        print("ERROR: attendees must be a list of dictionaries")
         return
 
-    for index, attendee in enumerate(attendees_list, start=1):
+    for index, attendee in enumerate(attendees, start=1):
         template_schema = template
         for key in ['name', 'event_title', 'event_date', 'event_location']:
             placeholder = "{" + f"{key}" + "}"
